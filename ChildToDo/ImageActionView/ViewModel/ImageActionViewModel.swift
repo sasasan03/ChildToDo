288d5b5ec79edf5d645d7aeb5593a436128d7e7d
sasasan03 <sako.hiro62@gmail.com>
2023-09-16 16:47:52 +0900

    [fix]: ImageActionViewModelのプロパティ修正とメソッドをToDoModelへコピー

--- ChildToDo/ImageActionView/ViewModel/ImageActionViewModel.swift
+++ ChildToDo/ImageActionView/ViewModel/ImageActionViewModel.swift
@@ -12,35 +12,30 @@ import AVFoundation
 
 class ImageActionViewModel: ObservableObject {
     
-    @Published var sharedDetailViewModel: DetailViewModel
     @Published var todo: ToDo
     @Published var todoDetail: ToDoDetail
+    @Published var toDos: [ToDo]
     
-    init(sharedDetailViewModel: DetailViewModel, todo: ToDo, todoDetail: ToDoDetail) {
-        self.sharedDetailViewModel = sharedDetailViewModel
+    private let toDoModel: ToDoModel
+    
+    init(todo: ToDo, todoDetail: ToDoDetail, toDoModel: ToDoModel) {
         self.todo = todo
         self.todoDetail = todoDetail
-    }
-    
-    //MARK: DetailViewModelの最新の情報を取得する。
-    var toDos: [ToDo]{
-        sharedDetailViewModel.toDos
+        self.toDoModel = toDoModel
+        
+        toDos = [ToDo(name: "", toDoDetails: [])]
+
+        toDoModel.$toDos
+            .assign(to: &$toDos)
     }
     
     //MARK: セルの『👍』アニメーションとセルの背景色（赤・青）の管理を行なっている。
     func isCheckedToChange(todo: ToDo, todoDetail: ToDoDetail){
-        guard let tIndex = sharedDetailViewModel.toDos.firstIndex(where: { $0.id == todo.id }) else { return }
-        guard let dIndex = todo.toDoDetails.firstIndex(where: { $0.id == todoDetail.id }) else { return }
-        sharedDetailViewModel.sharedHomeViewModel.toDos[tIndex].toDoDetails[dIndex].isChecked.toggle()
+        toDoModel.isCheckedToChange(todo: todo, todoDetail: todoDetail)
     }
     
     //MARK: 画面右上の『サークル』ボタンメソッド。isCheckedのBoolを全て『false』に切り替える。
     func isCheckedToAllFalse(todo: ToDo){
-        guard let tIndex = sharedDetailViewModel.toDos.firstIndex(where: { $0.id == todo.id }) else { return }
-        sharedDetailViewModel.toDos[tIndex].toDoDetails.forEach{ _ in
-            (0..<sharedDetailViewModel.toDos[tIndex].toDoDetails.count).forEach{
-                sharedDetailViewModel.sharedHomeViewModel.toDos[tIndex].toDoDetails[$0].isChecked = false
-            }
-        }
+        toDoModel.isCheckedToAllFalse(todo: todo)
     }
 }
