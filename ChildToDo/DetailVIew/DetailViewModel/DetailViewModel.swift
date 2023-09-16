963214efc3ee1435f091e202381343fd7f55f5c8
sasasan03 <sako.hiro62@gmail.com>
2023-09-16 16:36:01 +0900

    [fix]: DetailViewModelの値取得方法を変更

--- ChildToDo/DetailVIew/DetailViewModel/DetailViewModel.swift
+++ ChildToDo/DetailVIew/DetailViewModel/DetailViewModel.swift
@@ -9,23 +9,27 @@ import Foundation
 
 class DetailViewModel: ObservableObject {
     
-    @Published var sharedHomeViewModel: HomeViewModel
     @Published var todo: ToDo
     @Published var todoDetail: ToDoDetail
     @Published var isAddView = false
     @Published var isEditView = false
+    @Published var toDos: [ToDo]
     
-    //MARK: HomeViewModelから最新のtoDos情報を取得
-    var toDos: [ToDo]{
-        sharedHomeViewModel.toDos
-    }
+    private let toDoModel: ToDoModel
     
-    init(sharedHomeViewModel: HomeViewModel, todo: ToDo, todoDetail: ToDoDetail, isShowAddView: Bool = false) {
-        self.sharedHomeViewModel = sharedHomeViewModel
+    init(todo: ToDo, todoDetail: ToDoDetail, isAddView: Bool = false, isEditView: Bool = false, toDoModel: ToDoModel) {
         self.todo = todo
         self.todoDetail = todoDetail
-        self.isAddView = isShowAddView
+        self.isAddView = isAddView
+        self.isEditView = isEditView
+        self.toDoModel = toDoModel
+        
+        toDos = [ToDo(name: "", toDoDetails: [])]
+        
+        toDoModel.$toDos
+            .assign(to: &$toDos)
     }
+    
     
     //MARK: AddViewを開かせる
     func isShowAddView(){
@@ -39,35 +43,21 @@ class DetailViewModel: ObservableObject {
     
     //MARK: todoDetailの項目を削除
     func deleteTodoDetail(todo: ToDo, todoDetail: ToDoDetail, offset: IndexSet)  {
-        guard let todoIndex = sharedHomeViewModel.toDos.firstIndex(where: { $0.id == todo.id }) else { return }
-        sharedHomeViewModel.toDos[todoIndex].toDoDetails.remove(atOffsets: offset)
+        toDoModel.deleteTodoDetail(todo: todo, todoDetail: todoDetail, offset: offset)
     }
     
     //MARK: todoDetailの項目を移動
     func moveTodoDetail(indexSet: IndexSet, index: Int, todo: ToDo){
-        guard let todoIndex = sharedHomeViewModel.toDos.firstIndex(where: { $0.id == todo.id }) else { return }
-        sharedHomeViewModel.toDos[todoIndex].toDoDetails.move(fromOffsets: indexSet, toOffset: index)
+        toDoModel.moveTodoDetail(indexSet: indexSet, index: index, todo: todo)
     }
 
     //MARK: toDosに入力された値を追加する
     func addTodoDetail(text: String, todo: ToDo) throws {
-        if let index = toDos.firstIndex(of: todo){
-            var updatedToDo = todo
-            guard text != "" else {
-                throw NonTextError.nonTodoDetailText
-            }
-            updatedToDo.toDoDetails.append(ToDoDetail(name: text, isChecked: false))
-            sharedHomeViewModel.toDos[index] = updatedToDo
-        }
+        try toDoModel.addTodoDetail(text: text, todo: todo)
     }
     
     //MARK: toDosのセルを選択し、新しく入力された値を上書きする
     func todoDetailSave(newTodoDetail: ToDoDetail, todo: ToDo, newName: String) throws {
-        guard let index = toDos.firstIndex(where: { $0.id == todo.id }) else { return }
-        guard  let dIndex = todo.toDoDetails.firstIndex(where: { $0.id == newTodoDetail.id }) else { return }
-        guard newName != "" else {
-            throw NonTextError.nonTodoDetailText
-        }
-        sharedHomeViewModel.toDos[index].toDoDetails[dIndex].name = newName
+        try toDoModel.todoDetailSave(newTodoDetail: newTodoDetail, todo: todo, newName: newName)
     }
 }
